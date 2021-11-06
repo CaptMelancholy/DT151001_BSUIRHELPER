8187df54744adfb6e1422b948e396881b6aa8ed3
CaptMelancholy <clfabravo@yahoo.com>
2021-11-06 21:47:15 +0300

    UPDATE and NEW

---
+++ src/main/test/com.example.bsuir_helper_1/GameControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.bsuir_helper_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void moveSquareKeyPressed() {
+    }
+
+    @Test
+    void checkIfGameIsOver() {
+    }
+
+    @Test
+    void snakeHitItSelf() {
+    }
+}
---
+++ src/main/test/com.example.bsuir_helper_1/MainTest.java
@@ -0,0 +1,16 @@
+package com.example.bsuir_helper_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
