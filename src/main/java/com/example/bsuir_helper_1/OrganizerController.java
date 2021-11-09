0e72c74e562453c7700a23deedabe4d5a59171d1
Arina <92207451+rinrisha@users.noreply.github.com>
2021-11-09 18:23:27 +0300

    UPDATE and NEW

--- src/main/java/com/example/bsuir_helper_1/OrganizerController.java
+++ src/main/java/com/example/bsuir_helper_1/OrganizerController.java
@@ -1,6 +1,36 @@
 package com.example.bsuir_helper_1;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TreeView;
 
 public class OrganizerController {
+
+    @FXML
+    private MenuItem AboutMenuItem;
+
+    @FXML
+    private Menu ChangeMenu;
+
+    @FXML
+    private MenuItem DeleteMenuItem;
+
+    @FXML
+    private Menu HelpMenu;
+
+    @FXML
+    private MenuBar MenuBar;
+
+    @FXML
+    private SplitPane SplitPane;
+
+    @FXML
+    private TreeView<?> TreeView;
+
+    @FXML
+    private MenuItem UndoMenuItem;
 
 }
