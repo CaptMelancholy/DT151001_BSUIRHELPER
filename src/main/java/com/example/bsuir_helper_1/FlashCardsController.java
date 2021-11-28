a7a5184441f41b4a2fc64acbfe6c08158968ed62
vashk <vashkevichartem@mail.ru>
2021-11-28 11:07:03 +0300

    flash-cards almost complete

--- src/main/java/com/example/bsuir_helper_1/FlashCardsController.java
+++ src/main/java/com/example/bsuir_helper_1/FlashCardsController.java
@@ -17,16 +17,35 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.Objects;
 
 public class FlashCardsController {
+
     @FXML
     public Tab nameOfTopic;
 
     @FXML
     private Text food;
+
+    @FXML
+    private Text newCard;
+
+    @FXML
+    private Text newText1;
+
+    @FXML
+    private Text newText2;
+
+    @FXML
+    private Text newText3;
+
+    @FXML
+    private Text newText4;
+
+    @FXML
+    private Text newText5;
 
     @FXML
     private Text classes;
@@ -52,6 +71,36 @@ public class FlashCardsController {
     @FXML
     private AnchorPane listBackground;
 
+    @FXML
+    private Button deleteButton1;
+
+    @FXML
+    private Button deleteButton2;
+
+    @FXML
+    private Button deleteButton3;
+
+    @FXML
+    private Button deleteButton4;
+
+    @FXML
+    private Button deleteButton5;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button nameOfTabButton;
+
+    @FXML
+    private Button addNewButton;
+
+    @FXML
+    private Button listOfFlashcardsButton;
+
     final public String [] listOfFood = new String[] {"сталовка 4к","сталовка 2к","буфет 4к","буфет 5к","церковная лавка","продукты бар","корона","буфут 2к"};
     final public int numberOfFood = Array.getLength(listOfFood);
     final public String [] listOfClasses = new String[] {"лабы/учеба","фильмец","накотить встречу с друзьями","прогуляться по улице","погамать в компик","заняться свои хобби"};
@@ -59,6 +108,39 @@ public class FlashCardsController {
 
     public String [] listOfNewFlashcards = new String[15];
     public int numOfNewFlashcards = 0;
+    public int numberOfScroll;
+    public int nowNumberOfScroll = 0;
+
+    public String nameOfTab;
+    public String newFlashcard;
+
+    public File fileNameOfTab = new File("fileNameOfTab.txt");
+    public File newFlashcards = new File("newFlashcards.txt");
+
+    @FXML
+    private void initialize() {
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileNameOfTab));
+            nameOfTab = reader.readLine();
+            nameOfTopic.setText(nameOfTab);
+            reader.close();
+            BufferedReader readerOfNewFlashcards = new BufferedReader(new FileReader(newFlashcards));
+            while((newFlashcard = readerOfNewFlashcards.readLine()) != null){
+                if(!newFlashcard.equals("")){
+                listOfNewFlashcards[numOfNewFlashcards] = newFlashcard;
+                numOfNewFlashcards++;}
+            }
+            numberOfScroll = (numOfNewFlashcards-1)/5;
+            if(numberOfScroll>0) nextButton.setDisable(false);
+            newText1.setText(listOfNewFlashcards[nowNumberOfScroll*5]);
+            newText2.setText(listOfNewFlashcards[nowNumberOfScroll*5+1]);
+            newText3.setText(listOfNewFlashcards[nowNumberOfScroll*5+2]);
+            newText4.setText(listOfNewFlashcards[nowNumberOfScroll*5+3]);
+            newText5.setText(listOfNewFlashcards[nowNumberOfScroll*5+4]);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void generateNewPlace(){
         int newRandFood = (int) (Math.random() *numberOfFood);
@@ -71,15 +153,22 @@ public class FlashCardsController {
         classes.setText(listOfClasses[newRandClasses]);
     }
 
-    /*public void ChangeNameOfTab(){
-        SettingsChangeNameOfTab.setNameOfTab();
-    }*/
-
-
+    public void generateNewFlashcard(){
+        int newRandClasses = (int) (Math.random() *numOfNewFlashcards);
+        if (numOfNewFlashcards != 0) newCard.setText(listOfNewFlashcards[newRandClasses]);
+    }
 
     public void settings(){
     mainWindowBackground.setVisible(false);
     settingsWindowBackground.setVisible(true);
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileNameOfTab));
+            nameOfTab = reader.readLine();
+            nameOfTopic.setText(nameOfTab);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void closeButton(){
@@ -88,8 +177,17 @@ public class FlashCardsController {
     }
 
     public void saveButton(){
-        if (!Objects.equals(newNameOfTab.getText(), ""))
+        if (!Objects.equals(newNameOfTab.getText(), "")){
         nameOfTopic.setText(newNameOfTab.getText());
+            try{
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileNameOfTab));
+                fileNameOfTab = null;
+                writer.write(newNameOfTab.getText());
+                writer.flush();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         if (numOfNewFlashcards>14){
             Stage window = new Stage();
             window.initModality(Modality.APPLICATION_MODAL);
@@ -112,6 +210,25 @@ public class FlashCardsController {
         else{
             listOfNewFlashcards[numOfNewFlashcards] = addNew.getText();
             numOfNewFlashcards++;
+            numberOfScroll = (numOfNewFlashcards-1)/5;
+            if(numberOfScroll>0) nextButton.setDisable(false);
+            switch ((numOfNewFlashcards-1)%5) {
+                case (0) -> newText1.setText(addNew.getText());
+                case (1) -> newText2.setText(addNew.getText());
+                case (2) -> newText3.setText(addNew.getText());
+                case (3) -> newText4.setText(addNew.getText());
+                case (4) -> newText5.setText(addNew.getText());
+                default -> System.out.println("error");
+            }
+            try{
+                BufferedWriter writerOfNewFlashcards = new BufferedWriter(new FileWriter(newFlashcards,true));
+                writerOfNewFlashcards.write(addNew.getText()+"\n");
+                writerOfNewFlashcards.flush();
+                writerOfNewFlashcards.close();
+                addNew.setText("");
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -131,6 +248,48 @@ public class FlashCardsController {
         nameOfTabBackground.setVisible(false);
         addNewBackground.setVisible(false);
         listBackground.setVisible(true);
+    }
+
+    public void deleteButton1(){
+
+    }
+
+    public void deleteButton2(){
+
+    }
+
+    public void deleteButton3(){
+
+    }
+
+    public void deleteButton4(){
+
+    }
+
+    public void deleteButton5(){
+
+    }
+
+    public void backButton(){
+        nowNumberOfScroll--;
+        newText1.setText(listOfNewFlashcards[nowNumberOfScroll*5]);
+        newText2.setText(listOfNewFlashcards[nowNumberOfScroll*5+1]);
+        newText3.setText(listOfNewFlashcards[nowNumberOfScroll*5+2]);
+        newText4.setText(listOfNewFlashcards[nowNumberOfScroll*5+3]);
+        newText5.setText(listOfNewFlashcards[nowNumberOfScroll*5+4]);
+        nextButton.setDisable(false);
+        if(nowNumberOfScroll==0) backButton.setDisable(true);
+    }
+
+    public void nextButton(){
+        nowNumberOfScroll++;
+        newText1.setText(listOfNewFlashcards[nowNumberOfScroll*5]);
+        newText2.setText(listOfNewFlashcards[nowNumberOfScroll*5+1]);
+        newText3.setText(listOfNewFlashcards[nowNumberOfScroll*5+2]);
+        newText4.setText(listOfNewFlashcards[nowNumberOfScroll*5+3]);
+        newText5.setText(listOfNewFlashcards[nowNumberOfScroll*5+4]);
+        backButton.setDisable(false);
+        if(nowNumberOfScroll==numberOfScroll) nextButton.setDisable(true);
     }
 
     public void BackToMenuFlashCards(final ActionEvent event) throws IOException
